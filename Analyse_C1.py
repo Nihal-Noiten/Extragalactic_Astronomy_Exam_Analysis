e1201abf486b07b80325350d218ffcf33b752d95
Pier <andrea.pierini.hnr@gmail.com>
2021-02-13 22:19:08 +0100

    Added functions

--- Analyse_C1.py
+++ Analyse_C1.py
@@ -19,4 +19,127 @@ from	scipy.optimize			import curve_fit
 from	scipy.integrate 		import quad, ode
 # from scipy.special import erf
 
+#######################################################################################################
 
+# LATEX: ON, MNRAS template
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",				# or sans-serif
+    "font.serif": ["Times New Roman"]})	# or Helvetica
+
+#######################################################################################################
+
+# FUNCTIONS
+
+# The tree inverse CDFs for the homogeneous sphere:
+
+def R_P(P,a):
+	return a * np.cbrt(P)
+
+def Ph_P(P):
+	return 2 * np.pi * P
+
+def Th_P(P):
+	return np.arccos(1. - 2. * P)
+
+# The tree PDFs for the homogeneous sphere:
+
+def pdf_r(r,a):
+	return 3. * r**2 / (a**3)
+
+def pdf_ph(ph):
+	return 0.5 / np.pi * (1. + 0. * ph)
+
+def pdf_th(th):
+	return 0.5 * np.sin(th)
+
+# The circular velocity of the Plummer sphere, and derivative formulae
+
+def v_circ(r):
+	return r * np.sqrt( rho * 4. * np.pi / 3. )
+
+def v_case_1(r):
+	return 0.05 * v_circ(r)
+
+def v_case_2(r):
+	return 0.05 * a * v_circ(a) / r
+
+def pdf_v_1(v):
+	k = 0.05 * np.sqrt( rho * 4. * np.pi / 3. )
+	return 3. / ( (k * a * V0)**3 ) * v**2 
+
+def pdf_v_2(v):
+	k = 0.05 * np.sqrt( rho * 4. * np.pi / 3. ) * (a**2) * V0 * R0
+	return 3. * (k**3) / (a**3 * R0**3) / (v**4)
+
+def pdf_l_1(l):
+	k = 0.05 * np.sqrt( rho * 4. * np.pi / 3. ) * V0 / R0
+	return 1.5 / (a**3 * R0**3) / (k**(3./2.)) * l**(1./2.) 
+
+# A plotting primer for PDFs over histograms
+
+def histo_pdf_plotter(ax, x_lim, x_step, x_bins, func, npar, x_min=0.):
+	x = np.linspace(x_min, x_lim, 10000)
+	if npar == 1:
+		f_x = func(x, a * R0)
+		ax.plot(x, f_x, color='black', lw=0.9)
+	elif npar == 0:
+		f_x = func(x)
+		ax.plot(x, f_x, color='black', lw=0.9)
+	for j in range(len(x_bins)):
+		x = []
+		f_x = []
+		e_x = []
+		x_mid = x_bins[j] + x_step / 2
+		for i in range(9):
+			x_temp = x_bins[j] + x_step / 2 + x_step / 15 * (i-4)
+			if npar == 1:
+				f_temp = func(x_mid, a * R0)
+			elif npar == 0:
+				f_temp = func(x_mid)
+			e_temp = np.sqrt(f_temp * N) / N
+			ax.vlines(x_mid, ymin=f_temp-e_temp, ymax=f_temp+e_temp, color='black', lw=0.9)
+			x.append(x_temp)
+			f_x.append(f_temp)
+			e_x.append(e_temp)
+		x = np.array(x)
+		f_x = np.array(f_x)
+		e_x = np.array(e_x)
+		ax.plot(x, f_x + e_x, color='black', lw=0.9)
+		ax.plot(x, f_x - e_x, color='black', lw=0.9)
+
+def histo_pdf_plotter_log(ax, x_min, x_max, x_step, x_bins, func, npar):
+	log_min = np.log10(x_min)
+	log_max = np.log10(x_max)
+	x = np.logspace(log_min, log_max, 1000)
+	if npar == 1:
+		f_x = func(x, a * R0) # * N
+		ax.plot(x, f_x, color='black', lw=0.9)
+	elif npar == 0:
+		f_x = func(x) # * N
+		ax.plot(x, f_x, color='black', lw=0.9)
+	for j in range(len(x_bins)-1):
+		x = []
+		f_x = []
+		e_x = []
+		x_mid = (x_bins[j+1] + x_bins[j]) / 2.
+		x_step = x_bins[j+1] - x_bins[j]
+		for i in range(9):
+			x_temp = x_mid + x_step / 15 * (i-4)
+			if npar == 1:
+				f_temp = func(x_mid, a * R0) #* N
+			elif npar == 0:
+				f_temp = func(x_mid) # * N
+			e_temp = np.sqrt(f_temp * N) / N # np.sqrt(f_temp) # 
+			ax.vlines(x_mid, ymin=f_temp-e_temp, ymax=f_temp+e_temp, color='black', lw=0.9)
+			x.append(x_temp)
+			f_x.append(f_temp)
+			e_x.append(e_temp)
+		x = np.array(x)
+		f_x = np.array(f_x)
+		e_x = np.array(e_x)
+		ax.plot(x, f_x + e_x, color='black', lw=0.9)
+		ax.plot(x, f_x - e_x, color='black', lw=0.9)
+
+#######################################################################################################
